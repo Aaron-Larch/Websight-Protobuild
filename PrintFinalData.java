840e9171ef8b6c87bab024f310146491ca7793c5
brodewal <brodewal@gmu.edu>
2019-07-25 13:35:05 -0400

    Modified histogram setup
    Changed the implementation of the histogram generator to fix data retention bug.
    Replaced HashMap with LinkedHashMap.

---
+++ PrintFinalData.java
@@ -0,0 +1,132 @@
+/**
+ * 
+ */
+package JavaCallFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javaDemo.Reports;
+import javaDemo.Statistics;
+
+/**
+ * @author gce
+ *http://localhost:8080/RestApiJerseyTest/PrintFinalData
+ */
+@WebServlet("/PrintFinalData")
+public class PrintFinalData extends HttpServlet{
+	Map<String, Double> chartinfo;
+	public int i=0;
+	public static Reports[] statement;
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {   
+		DisplayPage(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {  
+		int value =Integer.parseInt(request.getParameter("action"));
+		if(i>=0 && i<statement.length) {
+			i += value;
+			if(i==-1) {i++;}//make sure the back value can never go below 0
+			else if(i==statement.length) {i--;}//make sure the next value can never go above max stored value
+		}
+		DisplayPage(request, response);
+		request.getRequestDispatcher("/WEB-INF/ChartBuild.jsp").forward(request, response);
+	}
+	
+	private void DisplayPage(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	    statement = (Reports[]) request.getSession().getAttribute("Final");
+		double[] bellCurveGraph=new double[statement[i].getlowC().length];
+		chartinfo= new HashMap<String, Double>();
+		int[] Xaxis =new int[statement[i].getlowC().length+1];
+		double[] BoxPlot=new double[5];
+		
+		for(int j=0; j<Xaxis.length; j++) {Xaxis[j]=j;}
+		
+		//Create a stream to hold the output
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		// IMPORTANT: Save the old System.out!
+		PrintStream old = System.out;
+		// Tell Java to use your special stream
+		System.setOut(ps);
+		// Print some output: goes to your special stream
+		statement[i].showRecord();
+		chartinfo=Statistics.SampleVariance(statement[i]);
+		chartinfo.putAll(Statistics.Range(statement[i]));
+		//chartinfo.putAll(Statistics.HistogramTable(statement[Math.abs(i)]));
+		// Put things back
+		System.out.flush();
+		System.setOut(old);
+		
+		BoxPlot[0]=statement[i].getmin();
+		BoxPlot[1]=chartinfo.get("lower interquartile range");
+		BoxPlot[2]=chartinfo.get("upper interquartile range");
+		BoxPlot[3]=statement[i].getmax();
+		BoxPlot[4]=statement[i].getmedian();
+		
+		double part1 = 1/(chartinfo.get("Standard Deveation") * Math.sqrt(2 * Math.PI));
+		for(int j=0; j<statement[i].getlowC().length; j++) {
+			double part2= Math.pow((statement[i].getlowC()[j]-statement[i].getaverage()),2);
+			double part3= 2*chartinfo.get("Sample Variance");
+			bellCurveGraph[j]=part1*Math.exp(-1*part2 /part3); //*statement[i].getlowC().length; 
+		}
+		
+		/*
+		 * Determine the max Y axis length.
+		 */
+		
+		int binWidth = 5;
+		
+		Map<Integer,Integer> currentHisto = Statistics.buildHisto(statement[i], binWidth);
+		
+		int maxY = 0;
+		for (Integer i : currentHisto.keySet()) 
+			if (i > maxY)
+				maxY = i;
+
+		int numberOfBins = currentHisto.keySet().size();
+		
+		int[] barKeys = new int[numberOfBins];
+		int[] barValues = new int[numberOfBins];
+		
+		
+		
+		for (int i = 0; i <= numberOfBins-1; i++) {
+			barKeys[i] = i * binWidth;
+			barValues[i] = currentHisto.get(i * binWidth);
+		}
+		
+		/*int[][] bin = Statistics.BuildBins(statement[i]);
+		double[] barYaxis=new double[bin.length];
+		for(int i=0; i<bin.length; i++) {
+			barYaxis[i]=chartinfo.get(Arrays.toString(bin[i]));
+		}*/
+		
+		
+		//print output
+		request.setAttribute("Message", baos.toString());
+		request.setAttribute("Label", statement[i].getreportId());
+		request.setAttribute("Xaxis", Xaxis);
+		request.setAttribute("YaxisHigh", statement[i].gethighC());
+		request.setAttribute("YaxisLow", statement[i].getlowC());
+		request.setAttribute("BellCurveGraph", bellCurveGraph);
+		request.setAttribute("BoxAndWhiskersGraph", BoxPlot);
+		request.setAttribute("Histogram", barValues);
+		request.setAttribute("BarGraph", barKeys);
+	}
+}
